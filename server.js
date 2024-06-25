f5919114207b4e63d4bb47b205ad60f8e44b171f
pawareliza14 <pawareliza14@gmail.com>
2024-06-25 10:30:29 +0530

    initial commit

--- server.js
+++ server.js
@@ -1,5 +1,27 @@
+const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app); // Create an HTTP server with the Express app
+const io = socketio(server); // Attach Socket.io to the HTTP server
 
-const PORT = 3000 || process.env.PORT;
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Run when a client connects
+io.on('connection', socket => {
+    console.log(`New WS connection`);
+
+    socket.emit('message','Welcome to ChatCord!');
+});
+
+// Use environment variable PORT or default to 3000
+const PORT = process.env.PORT || 3000;
+
+
+// Start server
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
