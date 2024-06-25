d740cd89a8a01577fed20e2c467f535a73998800
pawareliza14 <pawareliza14@gmail.com>
2024-06-25 11:59:48 +0530

    initial commit

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const formatMessage = require('./utils/messages')
 
 const app = express();
 const server = http.createServer(app); // Create an HTTP server with the Express app
@@ -10,11 +11,29 @@ const io = socketio(server); // Attach Socket.io to the HTTP server
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+const botName = 'ChatCord Bot';
+
 // Run when a client connects
-io.on('connection', socket => {
+io.on('connection', (socket) => {
     console.log(`New WS connection`);
 
-    socket.emit('message','Welcome to ChatCord!');
+    socket.emit('message',formatMessage(botName,'Welcome to ChatCord!'));
+
+    //Broadcast when a user connects
+    socket.broadcast.emit('message',formatMessage(botName,'A user has joined the chat'));
+    //{Broadcast basically means to everyone in the expect the user themselves}
+  
+
+    //Runs when client disconnects
+    socket.on('disconnect',() => {
+        io.emit('message',formatMessage(botName,'A user has left the chat'));
+    });
+
+    // Listen for chatMessage
+    socket.on('chatMessage', msg => {
+        io.emit('message',formatMessage('USER',msg))
+    });
+
 });
 
 // Use environment variable PORT or default to 3000
